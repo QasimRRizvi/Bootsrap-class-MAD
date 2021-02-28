ccc2b54092e36966f17fa3b5d76c1b6efb356d18
QasimRRizvi <syed.rizvi2385@gmail.com>
2021-02-28 14:59:54 +0500

    Added full name creation logic via function

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function pow(a, b) {
 
 const val = pow(2, 3);
 console.log(val);
+
+function getFullName(firstName, lastName, middleName="") {
+  return firstName + " " + middleName + " " + lastName;
+}
+
+const fullName = getFullName("Qasim", "Rizvi", "Raza");
+console.log(fullName)
